74aec3af9b1d753028d9b7f66ff038497ee613e3
Panu Tangchalermkul <panuta@gmail.com>
2010-01-13 16:32:45 +0700

    modify report/models.py to support approval process

--- report/models.py
+++ report/models.py
@@ -5,6 +5,7 @@ class Report(models.Model):
 	name = models.CharField(max_length=256)
 	description = models.CharField(max_length=1024, blank=True)
 	has_schedule = models.BooleanField(default=False)
+	need_approval = models.BooleanField(default=False)
 	created = models.DateTimeField(auto_now_add=True)
 	created_by = models.ForeignKey('domain.UserAccount')
 
@@ -16,7 +17,18 @@ class ReportSchedule(models.Model):
 	report_project = models.ForeignKey('ReportProject')
 	due_date = models.DateField()
 	is_submitted = models.BooleanField(default=False)
-	submitted = models.DateTimeField(null=True)
+	last_activity = models.IntegerField(default=ReportScheduleActivity.SUBMIT_ACTIVITY)
+	
+
+class ReportScheduleActivity(models.Model):
+	schedule = models.ForeignKey('ReportSchedule')
+	activity = models.IntegerField()
+	created_on = models.DateTimeField(auto_now_add=True)
+	created_by = models.ForeignKey('domain.UserAccount')
+	
+	SUBMIT_ACTIVITY = 1
+	APPROVE_ACTIVITY = 2
+	REJECT_ACTIVITY = 3
 
 class ReportScheduleFileResponse(models.Model):
 	schedule = models.ForeignKey('ReportSchedule')
