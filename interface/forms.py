4bfc0ce781bcb728f87aa37dd62f9cec001c882d
Siriwat Aumngamsup <lazyM0m3nt@gmail.com>
2010-01-27 11:37:19 +0700

    fix forms.py bug

--- interface/forms.py
+++ interface/forms.py
@@ -32,16 +32,16 @@ class UserAccountForm(forms.Form):
 class ChangeFirstTimePasswordForm(forms.Form):
 	password1 = forms.CharField(widget=forms.PasswordInput(), max_length=100, label='รหัสผ่าน')
 	password2 = forms.CharField(widget=forms.PasswordInput(), max_length=100, label='ยืนยันรหัสผ่าน')
-	
+
 	def clean(self):
 		cleaned_data = self.cleaned_data
 		password1 = cleaned_data.get('password1')
 		password2 = cleaned_data.get('password2')
-		
+
 		if password1 != password2:
 			self._errors["password2"] = ErrorList(['รหัสผ่านไม่สัมพันธ์กัน'])
 			del cleaned_data["password2"]
-		
+
 		return cleaned_data
 
 class ChangePasswordForm(forms.Form):
@@ -49,20 +49,20 @@ class ChangePasswordForm(forms.Form):
 	old_password = forms.CharField(widget=forms.PasswordInput(), max_length=100, label='รหัสผ่านเก่า')
 	new_password1 = forms.CharField(widget=forms.PasswordInput(), max_length=100, label='รหัสผ่านใหม่')
 	new_password2 = forms.CharField(widget=forms.PasswordInput(), max_length=100, label='ยืนยันรหัสผ่านใหม่')
-	
+
 	def clean(self):
 		cleaned_data = self.cleaned_data
 		username = cleaned_data.get('username')
 		old_password = cleaned_data.get('old_password')
 		new_password1 = cleaned_data.get('new_password1')
 		new_password2 = cleaned_data.get('new_password2')
-		
+
 		if new_password1 != new_password2:
 			self._errors["new_password2"] = ErrorList(['รหัสผ่านไม่สัมพันธ์กัน'])
 			del cleaned_data["new_password2"]
-		
+
 		from django.contrib.auth.models import User
-		
+
 		try:
 			user = User.objects.get(username=username)
 		except User.DoesNotExist:
@@ -70,11 +70,11 @@ class ChangePasswordForm(forms.Form):
 			del cleaned_data["username"]
 		else:
 			from django.contrib.auth.models import check_password
-			
+
 			if not check_password(old_password, user.password):
 				self._errors["old_password"] = ErrorList(['รหัสผ่านไม่ถูกต้อง'])
 				del cleaned_data["old_password"]
-		
+
 		return cleaned_data
 
 """
@@ -219,9 +219,18 @@ class UserAccountWizard(FormWizard):
 
 		return HttpResponseRedirect('/administer/users/')
 """
+# Model field
 class SectorChoiceField(forms.ModelChoiceField):
 	def label_from_instance(self, obj):
 		return "%d %s" % (obj.id, obj.name)
+
+class PlanChoiceField(forms.ModelChoiceField):
+	def label_from_instance(self, obj):
+		return "%s %s" % (obj.ref_no, obj.name)
+
+class ReportMultipleChoiceField(forms.ModelMultipleChoiceField):
+	def label_from_instance(self, obj):
+		return "%s" % obj.name
 
 class MasterPlanForm(forms.Form):
 	'''MasterPlan adding form'''
@@ -295,14 +304,6 @@ class MasterPlanEditProjectForm(forms.Form):
 class PlanForm(forms.Form):
 	ref_no = forms.CharField(max_length=512, label='รหัส')
 	name = forms.CharField(max_length=512, label='ชื่อกลุ่มแผนงาน')
-
-class PlanChoiceField(forms.ModelChoiceField):
-	def label_from_instance(self, obj):
-		return "%s %s" % (obj.ref_no, obj.name)
-
-class ReportMultipleChoiceField(forms.ModelMultipleChoiceField):
-	def label_from_instance(self, obj):
-		return "%s" % obj.name
 
 #
 # Project Form
